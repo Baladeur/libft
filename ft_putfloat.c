518acfa2ddbdbb9493204d5e1e111099d8a99a8e
Theo FERRIEUX <tferrieu@e1r13p14.42.fr>
2019-03-22 19:09:26 +0100

    update putfloat?

--- ft_putfloat.c
+++ ft_putfloat.c
@@ -6,13 +6,24 @@
 /*   By: tferrieu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/05 15:11:13 by tferrieu          #+#    #+#             */
-/*   Updated: 2019/03/05 15:11:58 by tferrieu         ###   ########.fr       */
+/*   Updated: 2019/03/10 18:37:04 by tferrieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putfloat(long double n)
+static int	count_dec(long double n)
+{
+	long double	k;
+	int			nb;
+
+
+	k = n;
+	while (k % 1 != 0)
+
+}
+
+void		ft_putfloat(long double n)
 {
 	if (n < 0.0)
 	{
